bad2a0995df30770ae8a1741069b59dbf2858dbc
Maverick Shreeve <maverickshreeve@gmail.com>
2024-08-14 18:24:50 -0700

    try another method for google auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { AppProps } from 'next/app';
+import Head from 'next/head'; // Import Head from next/head
 import { ThemeProvider } from '../context/ThemeContext';
 import { AuthProvider } from '../context/AuthContext';
 import Layout from '../components/Layout';
@@ -19,6 +20,10 @@ function MyApp({ Component, pageProps }: AppProps) {
     <ThemeProvider>
       <AuthProvider>
         <Layout>
+          <Head>
+            {/* Google site verification meta tag here */}
+            <meta name="google-site-verification" content="hJI_1hGI3FwrcQuqJ8KsdZZ8Hf_xmNzfhQavkoYppac" />
+          </Head>
           <Component {...pageProps} />
         </Layout>
       </AuthProvider>
